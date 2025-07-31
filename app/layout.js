f5c67ef59e533825281df8cb3e6dd03755cd0e60
muhammadzulfadly <muhammadzulfadly@student.telkomuniversity.ac.id>
2025-07-31 19:45:16 +0700

    perbaikan

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,18 @@
 import PropTypes from "prop-types";
 
 export const metadata = {
-  title: 'Admin LimapoccoeDigital',
-  description: 'Website Resmi Desa Limmapocoe',
-}
+  title: "Admin LimapoccoeDigital",
+  icons: {
+    icon: "/logo.png",
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
 
 RootLayout.propTypes = {
--- app/page.js
+++ app/page.js
@@ -1,16 +1,5 @@
-// app/page.js
 import { redirect } from "next/navigation";
-import Head from "next/head";
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Admin Limapoccoe Digital</title>
-        <link rel="icon" type="image/png" href="https://limapoccoedigital.id/logo.png" />
-      </Head>
-      {/* Redirect ke halaman /auth */}
-      {redirect("/auth")}
-    </>
-  );
+  redirect("/auth");
 }
