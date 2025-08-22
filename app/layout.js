ae4862edfc5461ddce8b4d187d6e03088d50643f
muhammadzulfadly <muhammadzulfadly@student.telkomuniversity.ac.id>
2025-08-22 09:15:31 +0700

    perbaikan

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,10 @@ import PropTypes from "prop-types";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="x3noKRLIxWyin4AxcIX2IqDaZsEU8JzZsWhyU9j9fAI" />
+        <meta name="robots" content="noindex, nofollow" />
+      </head>
       <body>{children}</body>
     </html>
   );
