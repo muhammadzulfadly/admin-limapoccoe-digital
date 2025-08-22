da2919f4a4f7bcae776c007f3ab879950f49bc77
muhammadzulfadly <muhammadzulfadly@student.telkomuniversity.ac.id>
2025-08-22 09:10:28 +0700

    perbaikan

--- app/auth/head.js
+++ app/auth/head.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 
 <Head>
+  <meta name="google-site-verification" content="x3noKRLIxWyin4AxcIX2IqDaZsEU8JzZsWhyU9j9fAI" />
   <meta name="robots" content="noindex, nofollow" />
-</Head>
+</Head>;
